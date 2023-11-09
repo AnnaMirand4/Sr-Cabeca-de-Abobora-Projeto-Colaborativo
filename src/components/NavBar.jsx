77cff9b85fa740364e73abc2428258824cd510b5
Anna Caroline Miranda <annacarolinemm@gmail.com>
2023-11-09 11:09:49 -0300

    começando a editar o css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import {useState} from "react";
 import {TiThMenuOutline} from "react-icons/ti"
 import style from "../Styles/componentes/navBar.module.css"
-import logoImage from '../assets/Senhor Cabeça de Abóbora Logo.png'
+import logoImage from '../assets/Senhor Cabeça de Abóbora Nome com Fundo.png'
 import { FaSearch } from 'react-icons/fa';
 
 
@@ -14,19 +14,10 @@ const Navbar = () => {
     };
   
     return (
-      <nav>
-        <div className={style.logo}>
-        <img src={logoImage} alt="Logo do Site com o nome Senhor cabeça de abóbora e uma imagem de uma abobora de halloween com um chapéu de bruxa roxo" />
-        </div>
-        
-        <div className={style.searchIcon}>
-          <Link to='/pesquisa'>
-              <FaSearch />
-          </Link>
-        </div>
-  
-        <div className={style.menuIcon} onClick={handleMenuToggle}>
-          <TiThMenuOutline />
+      <nav className={style.container}>
+        <div className={style.menucontainer}>
+         <div className={style.menuIcon} onClick={handleMenuToggle}>
+          <TiThMenuOutline size={40} />
         </div>
   
         {menuOpen && (
@@ -37,6 +28,19 @@ const Navbar = () => {
             </ul>
           </div>
         )}
+        </div>
+
+        <div className={style.logo}>
+        <img src={logoImage} alt="Logo do Site com o nome Senhor cabeça de abóbora e uma imagem de uma abobora de halloween com um chapéu de bruxa roxo" />
+        </div>
+        
+        <div className={style.searchIcon}>
+          <Link to='/pesquisa'>
+              <FaSearch />
+          </Link>
+        </div>
+  
+       
       </nav>
     );
   };
