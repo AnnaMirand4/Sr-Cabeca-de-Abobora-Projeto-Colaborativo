605bf9b66013d32c5fe36af96a08ad98378614f5
Anna Caroline Miranda <annacarolinemm@gmail.com>
2023-11-13 19:09:23 -0300

    conserto do componente de pesquisarFilmes

--- src/components/PesquisarFilmes.jsx
+++ src/components/PesquisarFilmes.jsx
@@ -6,11 +6,9 @@ import styles from '../Styles/componentes/pesquisarFilmes.module.css';
 import Subtitle from './Subtitle';
 import Text from './Text';
 
-
-
 const PesquisarFilmes = () => {
   const [termoPesquisa, setTermoPesquisa] = useState('');
-  const [filmeEncontrado, setFilmeEncontrado] = useState(null);
+  const [filmesEncontrados, setFilmesEncontrados] = useState([]);
 
   const handlePesquisarFilme = async () => {
     try {
@@ -20,15 +18,16 @@ const PesquisarFilmes = () => {
         },
       });
 
-      const primeiroResultado =response.data[0];
-      
+      const todosOsFilmes = response.data;
+      const resultados = todosOsFilmes.filter((filme) =>
+        filme.titulo.toLowerCase().includes(termoPesquisa.toLowerCase())
+      );
 
-      setFilmeEncontrado(primeiroResultado);
-      console.log(response.data)
+      setFilmesEncontrados(resultados);
+      console.log(resultados);
     } catch (error) {
       console.error('Erro ao buscar filme:', error);
-      setFilmeEncontrado(null);
-      
+      setFilmesEncontrados([]);
     }
   };
 
@@ -36,42 +35,42 @@ const PesquisarFilmes = () => {
     if (termoPesquisa.trim() !== '') {
       handlePesquisarFilme();
     } else {
-      setFilmeEncontrado(null);
+      setFilmesEncontrados([]);
     }
   }, [termoPesquisa]);
 
   return (
     <>
-    <div className={styles.container}>
-      
-      <div className={styles.inputContainer}>
-      <FaSearch />
-        <label htmlFor="termoPesquisa">Pesquisar Filme:</label>
-        <input
-          type="text"
-          id="termoPesquisa"
-          value={termoPesquisa}
-          onChange={(e) => setTermoPesquisa(e.target.value)}
-        />
-       </div>
-      {termoPesquisa.trim() !== '' && filmeEncontrado ? (
-        <div className={styles.filmeContainer}>
-          <Subtitle content="Filme Encontrado!" />
-          <Link to={`/sobreFilme/${filmeEncontrado.id}`}>
-            <img
-              className={styles.posterFilme}
-              src={filmeEncontrado.poster}
-              alt={filmeEncontrado.titulo}
-            />
-          </Link>
-          <Subtitle content={filmeEncontrado.titulo} />
+      <div className={styles.container}>
+        <div className={styles.inputContainer}>
+          <FaSearch />
+          <label htmlFor="termoPesquisa">Pesquisar Filme:</label>
+          <input
+            type="text"
+            id="termoPesquisa"
+            value={termoPesquisa}
+            onChange={(e) => setTermoPesquisa(e.target.value)}
+          />
         </div>
-      ) : (
-        termoPesquisa.trim() !== '' && (
-          <Text content="Filme não encontrado no site" />
-        )
-      )}
-    </div>
+        {termoPesquisa.trim() !== '' && filmesEncontrados.length > 0 ? (
+          <div>
+            {filmesEncontrados.map((filme) => (
+              <div key={filme.id} className={styles.filmeContainer}>
+                <Link to={`/sobreFilme/${filme.id}`}>
+                  <img
+                    className={styles.posterFilme}
+                    src={filme.poster}
+                    alt={filme.titulo}
+                  />
+                </Link>
+                <Subtitle content={filme.titulo} />
+              </div>
+            ))}
+          </div>
+        ) : (
+          termoPesquisa.trim() !== '' && <Text content="Filme não encontrado no site" />
+        )}
+      </div>
     </>
   );
 };
